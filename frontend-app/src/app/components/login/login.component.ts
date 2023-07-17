d5228b2c5de5ca57378806a67fdd1705fd06c56d
David Ricardo <r.arteta@utp.edu.co>
2023-07-17 09:28:52 -0500

    ng-alert added

--- frontend-app/src/app/components/login/login.component.ts
+++ frontend-app/src/app/components/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Alert } from '../../utils/alert.interface';
+import { ALERTS } from '../../utils/const.interface';
+
+
 
 @Component({
   selector: 'app-login',
@@ -12,10 +16,13 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   loginError: boolean = false;
+  alerts: Alert[] = [];
 
   @ViewChild('loginForm') loginForm!: NgForm;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {
+    this.reset();
+   }
 
   login() {
     if (this.loginForm && this.loginForm.valid) {
@@ -29,4 +36,12 @@ export class LoginComponent {
       });
     }
   }
+
+  close(alert: Alert) {
+		this.alerts.splice(this.alerts.indexOf(alert), 1);
+	}
+
+	reset() {
+		this.alerts = Array.from(ALERTS);
+	}
 }
